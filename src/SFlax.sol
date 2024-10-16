561198d2f042d8bd759a02e55e7a4a5dc1ca1da2
gititgoro <admin@behodler.io>
2024-10-16 16:31:51 +0200

    added sflax interface

--- src/SFlax.sol
+++ src/SFlax.sol
@@ -2,10 +2,24 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+interface ISFlax is IERC20 {
+    function approvedBurners(address burner) external returns (bool);
+
+    function mint(address recipient, uint amount) external;
+
+    function burn(uint amount) external;
+
+    function burnFrom(address holder, uint amount) external;
+
+    function setApprovedBurner(address burner, bool canBurn) external;
+}
+
 ///@notice Reward token for staking Flax. Instantiated by CoreStaker. Burnt on usage
-contract SFlax is Ownable, ERC20 {
+contract SFlax is Ownable, ERC20, ISFlax {
     mapping(address => bool) public approvedBurners;
 
     constructor() Ownable(msg.sender) ERC20("Staked Flax", "SFlax") {}
